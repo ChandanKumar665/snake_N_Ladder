ff17cf9f3b6e35856c37734985f38e654023f40a
Chandan Kumar <chandan.kumar@global.loc>
2019-05-23 22:03:37 +0530

    - /index.js index.ejs -- implementated core game logic

--- public/js/index.js
+++ public/js/index.js
@@ -85,14 +85,17 @@ function init(){
 
 function loadGame(){
     player1 = 0;
-    player2 = 0;
+    player2 = 99;
     player1_initiated = false;
-    player2_initiated = false;
+    player2_initiated = true;
 
     $('#score1, #score2').text(0);
     $('#roll1').removeAttr('disabled',true);
     $('#roll2').attr('disabled',true);
     $('#dice_result').empty();
+    $('#restart_btn').hide();
+    $('.restart').hide();
+    $('#winning').show();
 }
 
 
@@ -107,6 +110,65 @@ function isWinner(score){
     return (score == 100)? true: false;
 }
 
+function isSnake(score){
+    // snake's position
+    snake = [
+            {head:24,tail:18},
+            {head:26,tail:16},
+            {head:28,tail:14},
+            {head:55,tail:34},
+            {head:57,tail:36},
+            {head:59,tail:38},
+            {head:91,tail:50},
+            {head:95,tail:74},
+            {head:97,tail:76},
+            {head:99,tail:78}
+        ];
+
+        if(score < snake[0].head){
+            return score;
+        }else{
+            for(var i in snake){
+                if(score == snake[i].head){
+                    return snake[i].tail;
+                }else if(score < snake[i].head){
+                    return score;
+                }
+            }
+
+        }
+    }
+
+    function isLadder(score){
+        //ladder's position
+        ladder = [
+            {bottom:5,up:17},
+            {bottom:7,up:15},
+            {bottom:9,up:13},
+            {bottom:33,up:47},
+            {bottom:35,up:45},
+            {bottom:37,up:43},
+            {bottom:40,up:81},
+            {bottom:64,up:83},
+            {bottom:66,up:85},
+            {bottom:68,up:87}
+            ];
+
+            if(score < ladder[0].bottom){
+                return [score,false];
+            }else if(score > ladder[9].bottom){
+                return [score,false];
+            }else{
+                for(var i in ladder){
+                    if(score == ladder[i].bottom){
+                        return [ladder[i].up,true];
+                    }else if(score < ladder[i].bottom){
+                        return [score,false];
+                    }
+                }
+            }
+    }
+
 $('.btn_roll').click(function(){
     var title = $(this).attr('title');
     var dice_no = rollDice();
@@ -115,17 +177,33 @@ $('.btn_roll').click(function(){
             //player 1 initiated
             player1_initiated = true
         }else if(player1_initiated){
-            player1 = ((player1 + dice_no) > 100) ? player1 : player1 + dice_no;
+            // player1 = ((player1 + dice_no) > 100) ? player1 : player1 + dice_no;
+            if((player1 + dice_no) > 100){
+                player1 = player1;
+            }else{
+                player1 = player1 + dice_no;
+                if(player1 != 100){
+                    var temp = isLadder(player1);
+                    if(!temp[1]){
+                        // if ladder is not found then check for snake.
+                        player1 = isSnake(player1);
+                    }else{
+                        //ladder is found
+                        player1 = temp[0];
+                    }
+                }
+                
+            }
             $('#score1').text(player1);
             if(isWinner(player1)){
                 $('#myModal').modal({
                     show:true
                 })
+                $('.modal-title').text('Congratulation');
                 $('#win_msg').text("Player 1 Won.");
-                return
+                // return
             }
         }
-
 
         $('#roll1').attr('disabled',true);
         $('#roll2').removeAttr('disabled',true);
@@ -135,26 +213,56 @@ $('.btn_roll').click(function(){
             //player 2 initiated
             player2_initiated = true
         }else if(player2_initiated){
-            player2 = ((player2 + dice_no) > 100) ? player2 : player2 + dice_no;
+            // player2 = ((player2 + dice_no) > 100) ? player2 : player2 + dice_no;
+            if((player2 + dice_no) > 100){
+                player2 = player2;
+            }else{
+                player2 = player2 + dice_no;
+                if(player2 != 100){
+                    var temp = isLadder(player2);
+                    if(!temp[1]){
+                        // if ladder is not found then check for snake.
+                        player2 = isSnake(player2);
+                    }else{
+                        //ladder is found
+                        player2 = temp[0];
+                    }
+                }
+            }
             $('#score2').text(player2);
             if(isWinner(player2)){
                 $('#myModal').modal({
                     show:true
                 })
+                $('.modal-title').text('Congratulation');
                 $('#win_msg').text("Player 2 Won.");
-                return
+                // return
             }
 
         }
         $('#roll2').attr('disabled',true);
         $('#roll1').removeAttr('disabled',true);
     }
-
+    $('#restart_btn').show();
 })
 
-$('#close').click(function(){
-    console.log('inside close')
+$('#close, #ok').click(function(){
+    // console.log('inside close')
     loadGame();
+})
+$('#restart_btn').click(function(){
+    $('#myModal').modal({
+        show:true
+    });
+    $('#winning').hide();
+    $('.restart').show();
+    $('.modal-title').text('Confirmation');
+    $('#win_msg').text('Are you sure you want exit the current game ?');
+});
+
+$('#cancel').click(function(){
+    $('#winning').show();
+    $('.restart').hide();
 })
 // init();
 loadGame();
